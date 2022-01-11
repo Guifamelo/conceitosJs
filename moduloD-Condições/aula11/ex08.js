db29611d473f81a7b2323321e25615cbac8a5411
Guilherme Faria de Melo <guilhermefdemelo@hotmail.com>
2022-01-11 20:49:45 -0300

    condicoes

---
+++ moduloD-Condições/aula11/ex08.js
@@ -0,0 +1,8 @@
+//para ativar o console do NODEJS, tecla F-8 , desabilitar F-9
+console.log('O console funcionou corretamente!')
+var vel = 6
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+console.log('Dirija sempre com cinto de seguranca!')
+if(vel > 60){
+    console.log('voce excedeu o limite. Tome multa!')
+}
---
+++ moduloD-Condições/aula11/ex09.js
@@ -0,0 +1,8 @@
+//para ativar o console do NODEJS, tecla F-8 , desabilitar F-9
+var pais = 'Brasi'
+console.log(`Natural de ${pais}, entao...`)
+if(pais != 'Brasil'){
+    console.log('voce eh gringo')
+}else{
+    console.log('voce eh Brazuka!')
+}
